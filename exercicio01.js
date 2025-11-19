1b0a5c46888bde972415503fd128e129d611ec92
Leonardo Santos <04831025801@imaccommand017.local>
2025-11-19 11:31:53 -0300

    Exercicio 01 Feito

---
+++ exercicio01.js
@@ -0,0 +1,22 @@
+let nome = (prompt("Qual o seu nome? "))
+
+ if (nome == "admin") {
+     console.log("Acesso Liberado")
+ }
+else{
+     console.log("Acesso Negado")
+}
+
+
+
+
+
+
+let senha = (prompt("Qual é sua senha? "))
+
+ if (senha == "senha123") {
+     console.log("Acesso Liberado")
+ }
+else{
+     console.log("Acesso Negado")
+}
