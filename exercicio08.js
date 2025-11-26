af23aa8978bd57809d01d4f3fb877e8a23e146c9
Leonardo Santos <04831025801@imaccommand017.local>
2025-11-26 14:36:29 -0300

    Exercicio 08

--- exercicio08.js
+++ exercicio08.js
@@ -28,15 +28,18 @@ let imc = peso / (altura * altura);
 alert("Seu IMC é: " + imc.toFixed(2));
 
 if (imc < 18.5) {
-    console.log("Abaixo do peso");
-} else if (imc < 25) {
+    alert("Abaixo do peso");
+} else if (imc < 18.5 && imc <= 24.9) {
     
-    console.log("Peso normal");
-} else if (imc < 30) {
-    console.log("Sobrepeso");
-} else if (imc < 35) {
-    console.log("Obesidade grau 1");
-} else if (imc < 40) {
-    console.log("Obesidade grau 2");
+    alert("Peso normal");
+} else if (imc >= 25 && imc <=29.9) {
+    alert("Sobrepeso");
+} else if (imc >= 30 && imc <=34.9) {
+    alert("Obesidade grau 1");
+} else if (imc >= 35 && imc <=39.9) {
+    alert("Obesidade grau 2");
 } else {
-    console.log("Obesidade grau 3");}
+    alert("Obesidade grau 3");}
+
+
+    
